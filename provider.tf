2d9965df9664a00cd19334fbbd13cf993546f51e
mounika <lingammounika2@gmail.com>
2025-05-21 14:28:20 +0530

    changed provider form

--- provider.tf
+++ provider.tf
@@ -1,13 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.0.0-beta1"
-    }
-  }
-}
-
 provider "aws" {
-  # Configuration options
-  region = "us-east-1"
+    region = "us-east-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+  
 }
--- variable.tf
+++ variable.tf
@@ -1,3 +1,13 @@
 variable "bucketname" {
     default = "myterraformprojectwebsite05212025"  
+}  
+
+variable "access_key" {
+    description = "please enter valid access key"
+  
+}
+
+variable "secret_key" {
+    description = "please enter valid secret key"
+  
 }
